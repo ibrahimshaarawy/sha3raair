f15f382df1e36902d1061b4b472b2c7624dc7c67
Ibrahim Mohamed Ragheb Ragheb Shaarawy <isha0004@student.monash.edu>
2021-03-08 00:48:43 +0200

    Added routing

--- sha3raair/config/routes.rb
+++ sha3raair/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+  	namespace :v1 do
+  		resources :flights, only [:create, :destroy]
+  		resources :planes, only[:create, :destroy]
+  		resources :admins, only[:create, :destroy]
+  		resources :seats, only[:create, :destroy]
+  	end
+  end
+
+  get '*path', to: 'pages#index', via: :all
+
+
 end
